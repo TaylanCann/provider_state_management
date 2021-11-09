afd2d705a459b4107689e724c6689c06bc31c6ce
TaylanCann <taylancanh@gmail.com>
2021-11-10 00:05:28 +0300

    Initial commit

---
+++ lib/Screens/MainScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  final title;
+  const MainScreen({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
